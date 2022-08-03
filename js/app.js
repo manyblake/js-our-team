d19de55dbd7ca76ed9a8e4497b8e28ff156d1d41
manyblake <manyblake@gmail.com>
2022-08-03 15:46:02 +0200

    bonus 2

--- js/app.js
+++ js/app.js
@@ -33,23 +33,54 @@ teamMates.push(
   }
 );
 
-const container = document.querySelector(`.container`);
-const membersList = document.createElement(`ul`);
-container.appendChild(membersList);
+// const container = document.querySelector(`.container`);
+// const membersList = document.createElement(`ul`);
+// container.appendChild(membersList);
 
-for (i = 0; i < teamMates.length; i++) {
-  const li = document.createElement(`li`);
-  membersList.appendChild(li);
-  li.innerHTML =
-    `${teamMates[i].name}` + ` ${teamMates[i].role}` + ` ${teamMates[i].image}`;
-}
+// for (i = 0; i < teamMates.length; i++) {
+//   const li = document.createElement(`li`);
+//   membersList.appendChild(li);
+//   li.innerHTML =
+//     `${teamMates[i].name}` + ` ${teamMates[i].role}` + ` ${teamMates[i].image}`;
+// }
+
+const row = document.querySelector(`.row`);
 
 function createImgtagFromString(string) {
-  const img = document.createElement("img");
-  img.src = `./img/${string}`;
-  return img;
+  imgPath = `./img/${string}`;
+  return imgPath;
+}
+
+function createCard(name, role, img) {
+  const card = document.createElement(`div`);
+  card.className = `card`;
+
+  const imgBox = document.createElement(`div`);
+  imgBox.className = `card__imgbox`;
+  card.appendChild(imgBox);
+
+  const cardTitle = document.createElement(`h3`);
+  cardTitle.className = `card__title`;
+  cardTitle.innerHTML = name;
+  card.appendChild(cardTitle);
+
+  const cardDescription = document.createElement(`p`);
+  cardDescription.className = `card__description`;
+  cardDescription.innerHTML = role;
+  card.appendChild(cardDescription);
+
+  const cardImage = document.createElement(`img`);
+  cardImage.src = `${img}`;
+  cardImage.className = `card__image`;
+  imgBox.appendChild(cardImage);
+  return card;
 }
 
 for (i = 0; i < teamMates.length; i++) {
-  container.appendChild(createImgtagFromString(teamMates[i].image));
+  const img = createImgtagFromString(teamMates[i].image);
+  const name = teamMates[i].name;
+  const role = teamMates[i].role;
+  const card = createCard(name, role, img);
+
+  row.appendChild(card);
 }
